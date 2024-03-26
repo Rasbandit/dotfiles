c1a6157fc7a294f5f5ce88cd1034f9690f318911
rasbandit <todd.rasband@gmail.com>
2024-03-26 04:40:01 -0600

    2024-03-26 04:40

--- .setup-scripts/cron-jobs/auto-add-push.sh
+++ .setup-scripts/cron-jobs/auto-add-push.sh
@@ -2,17 +2,17 @@
 
 # Add changes to tracked files to the staging area
 
-/usr/bin/git --git-dir=/home/rasbandit/.cfg --work-tree=$HOME add -u
+/usr/bin/git --git-dir=/home/rasbandit/.cfg --work-tree=/home/rasbandit add -u
 
 # Check if there are changes in the staging area
-if [ -n "$(/usr/bin/git --git-dir=/home/rasbandit/.cfg --work-tree=$HOME diff --cached --name-only)" ]; then
+if [ -n "$(/usr/bin/git --git-dir=/home/rasbandit/.cfg --work-tree=/home/rasbandit diff --cached --name-only)" ]; then
     # Generate timestamp in the format YYYY-MM-DD-HH-MM
     timestamp=$(date +"%Y-%m-%d %H:%M")
 
     # Commit the changes with the specified commit message including the timestamp
-    /usr/bin/git --git-dir=/home/rasbandit/.cfg --work-tree=$HOME commit -m "$timestamp"
+    /usr/bin/git --git-dir=/home/rasbandit/.cfg --work-tree=/home/rasbandit commit -m "$timestamp"
     # Push the committed changes to the remote repository
-    /usr/bin/git --git-dir=/home/rasbandit/.cfg --work-tree=$HOME push
+    /usr/bin/git --git-dir=/home/rasbandit/.cfg --work-tree=/home/rasbandit push
 else
     echo "No changes to commit."
 fi
