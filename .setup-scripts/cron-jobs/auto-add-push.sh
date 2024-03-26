7e28c89f58a9407a20c85b5595d6cfa2b834929d
rasbandit <todd.rasband@gmail.com>
2024-03-26 12:49:12 -0600

    2024-03-26 12:49

--- .setup-scripts/cron-jobs/auto-add-push.sh
+++ .setup-scripts/cron-jobs/auto-add-push.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
 
-/usr/bin/git --git-dir=/home/rasbandit/.cfg --work-tree=/home/rasbandit pull
+set -ex
 
-/usr/bin/git --git-dir=/home/rasbandit/.cfg --work-tree=/home/rasbandit add -u
+function config() {
+    /usr/bin/git --git-dir=/home/rasbandit/.cfg --work-tree=/home/rasbandit $@
+}
+
+config pull
+
+config add /home/rasbandit/.setup-scripts/**/*
+
+config add -u
 
 # Check if there are changes in the staging area
-if [ -n "$(/usr/bin/git --git-dir=/home/rasbandit/.cfg --work-tree=/home/rasbandit diff --cached --name-only)" ]; then
+if [ -n "$(config diff --cached --name-only)" ]; then
     # Generate timestamp in the format YYYY-MM-DD-HH-MM
     timestamp=$(date +"%Y-%m-%d %H:%M")
 
     # Commit the changes with the specified commit message including the timestamp
     /usr/bin/git --git-dir=/home/rasbandit/.cfg --work-tree=/home/rasbandit commit -m "$timestamp"
     # Push the committed changes to the remote repository
-    /usr/bin/git --git-dir=/home/rasbandit/.cfg --work-tree=/home/rasbandit push
+    config push
 else
     echo "No changes to commit."
 fi
