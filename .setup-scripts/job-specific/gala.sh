afeb9741b1cd43a85027f6e82be0b118e01a261c
rasbandit <todd.rasband@gmail.com>
2024-03-27 17:50:04 -0500

    2024-03-27 17:50

--- .setup-scripts/job-specific/gala.sh
+++ .setup-scripts/job-specific/gala.sh
@@ -8,3 +8,5 @@ sudo snap install slack
 ~/.setup-scripts/apt/tailscale.sh
 ~/.setup-scripts/other-installs/aws-cli.sh
 ~/.setup-scripts/other-installs/kubectl.sh
+
+~/.setup-scripts/mount-secret-envar.sh "Gala" "Gala Envars"
--- .setup-scripts/mount-secret-envar.sh
+++ .setup-scripts/mount-secret-envar.sh
@@ -12,6 +12,9 @@ item_name=$2
 # Generate JSON using `op item get` command
 input_json=$(op item get "$item_name" --vault "$vault_name" --format json)
 
+# Define the secrets file path using the $HOME variable
+secrets_file="$HOME/.bash_secrets"
+
 # Function to add or update environment variables
 add_or_update_env() {
     local object=$1
@@ -20,7 +23,6 @@ add_or_update_env() {
 
     # Check if the value is null or empty
     if [[ $value == "null" || -z $value ]]; then
-        echo "Skipping $key because the value is empty"
         return
     fi
 
@@ -39,5 +41,13 @@ while IFS= read -r field; do
     add_or_update_env "$field"
 done <<< "$fields"
 
-# Append sourcing of the newly created .bash_secrets file to the main .bash_secrets file
-echo "source \"$HOME/.bash_secrets_$item_name\"" >> "$HOME/.bash_secrets"
+# Define the sourcing line for the current item
+sourcing_line="source \"\$HOME/.bash_secrets_$item_name\""
+
+# Check if the sourcing line already exists in the .bash_secrets file
+if ! grep -qF "$sourcing_line" "$secrets_file"; then
+    # Append sourcing of the newly created .bash_secrets file to the main .bash_secrets file
+    echo "$sourcing_line" >> "$secrets_file"
+fi
+
+source "$HOME/.bash_secrets"
