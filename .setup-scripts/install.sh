27af9eefcff11ba4d4a676c2d641ba2e1233bf99
rasbandit <todd.rasband@gmail.com>
2024-03-26 18:18:47 -0600

    2024-03-26 18:18

--- .setup-scripts/install.sh
+++ .setup-scripts/install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# Path to your script
-script_path="/home/${USER}/.setup-scripts/cron-jobs/auto-add-push.sh"
 
 # Check if the distribution is Ubuntu
 if [ "$(lsb_release -si)" = "Ubuntu" ]; then
     ~/.setup-scripts/operating-systems/ubuntu-setup.sh
 fi
 
+# Path to your script
+script_path="/home/${USER}/.setup-scripts/cron-jobs/auto-add-push.sh"
 cron_job="*/10 * * * * $script_path"
 echo "$cron_job" > /tmp/cron_job
 crontab /tmp/cron_job
