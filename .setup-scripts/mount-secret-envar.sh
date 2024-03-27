fca2f9524057cc89a9ff4c4be4963b253832dacd
rasbandit <todd.rasband@gmail.com>
2024-03-27 16:20:03 -0600

    2024-03-27 16:20

--- .setup-scripts/mount-secret-envar.sh
+++ .setup-scripts/mount-secret-envar.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 
+# Check if vault name and item name are provided as arguments
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <vault_name> <item_name>"
+    exit 1
+fi
+
+vault_name=$1
+item_name=$2
+
+# Generate JSON using `op item get` command
+input_json=$(op item get "$item_name" --vault "$vault_name" --format json)
+
 # Function to add or update environment variables
 add_or_update_env() {
     local key=$1
     local value=$2
 
     # Check if the value is empty
-    if [ -z "$value" ]; then
-        echo "Skipping empty value for $key"
+    if [[ -z $value ]]; then
+        echo "Skipping $key because the value is empty"
         return
     fi
 
@@ -22,12 +34,13 @@ add_or_update_env() {
 }
 
 # Parse JSON and loop over fields
-fields=$(echo "$input_json" | jq -r '.fields[]')
-while IFS= read -r field; do
-    label=$(echo "$field" | jq -r '.label')
-    value=$(echo "$field" | jq -r '.value')
+labels=$(echo "$input_json" | jq -r '.fields[].label')
+values=$(echo "$input_json" | jq -r '.fields[].value')
+
+# Iterate over the arrays in parallel
+while IFS= read -r label && IFS= read -r value; do
     add_or_update_env "$label" "$value"
-done <<< "$fields"
+done <<< "$(paste <(echo "$labels") <(echo "$values"))"
 
 # Source the file to apply changes immediately
 source "$HOME/.bash_secrets"
