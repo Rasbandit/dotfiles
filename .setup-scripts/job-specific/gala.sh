8c115c293d47559ebde8711d95b400c57441f18b
rasbandit <todd.rasband@gmail.com>
2024-03-27 17:20:03 -0600

    2024-03-27 17:20

--- .setup-scripts/job-specific/gala.sh
+++ .setup-scripts/job-specific/gala.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
-sudo apt update
+# sudo apt update
 
-sudo snap install slack
+# sudo snap install slack
 
-~/.setup-scripts/apt/lens.sh
-~/.setup-scripts/apt/tailscale.sh
-~/.setup-scripts/other-installs/kubectl.sh
+# ~/.setup-scripts/apt/lens.sh
+# ~/.setup-scripts/apt/tailscale.sh
+# ~/.setup-scripts/other-installs/kubectl.sh
 
-~/.setup-scripts/mount-secret-envar.sh "Gala" "Gala Envars"
+# ~/.setup-scripts/mount-secret-envar.sh "Gala" "Gala Envars"
 
-~/.setup-scripts/other-installs/aws-cli.sh
+# ~/.setup-scripts/other-installs/aws-cli.sh
+
+aws configure set aws_access_key_id "$(op item get "Gala AWS New" --fields label=access_key_id)" --profile chain
+aws configure set aws_secret_access_key "$(op item get "Gala AWS New" --fields label=secret_access_key)" --profile chain
+aws configure set region us-east-1 --profile chain
+aws configure set output json --profile chain
+
+aws configure set aws_access_key_id "$(op item get "Gala AWS" --fields label=access_key_id)"
+aws configure set aws_secret_access_key "$(op item get "Gala AWS" --fields label=secret_access_key)"
+aws configure set region us-east-1
+aws configure set output json
