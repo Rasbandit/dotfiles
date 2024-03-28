22657321dd5e5aaf850bd8a0e00384d831fc5cb8
rasbandit <todd.rasband@gmail.com>
2024-03-28 16:50:04 -0600

    2024-03-28 16:50

--- .setup-scripts/job-specific/gala.sh
+++ .setup-scripts/job-specific/gala.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-# sudo apt update
+sudo apt update
 
-# sudo snap install slack
+sudo snap install slack
 
-# ~/.setup-scripts/apt/lens.sh
-# ~/.setup-scripts/apt/tailscale.sh
-# ~/.setup-scripts/other-installs/kubectl.sh
+~/.setup-scripts/apt/lens.sh
+~/.setup-scripts/apt/tailscale.sh
+~/.setup-scripts/other-installs/kubectl.sh
 
-# ~/.setup-scripts/mount-secret-envar.sh "Gala" "Gala Envars"
+~/.setup-scripts/mount-secret-envar.sh "Gala" "Gala Envars"
 
-# ~/.setup-scripts/other-installs/aws-cli.sh
+~/.setup-scripts/other-installs/aws-cli.sh
 
 aws configure set aws_access_key_id "$(op item get "Gala AWS New" --fields label=access_key_id)" --profile chain
 aws configure set aws_secret_access_key "$(op item get "Gala AWS New" --fields label=secret_access_key)" --profile chain
@@ -21,3 +21,11 @@ aws configure set aws_access_key_id "$(op item get "Gala AWS" --fields label=acc
 aws configure set aws_secret_access_key "$(op item get "Gala AWS" --fields label=secret_access_key)"
 aws configure set region us-east-1
 aws configure set output json
+
+aws eks update-kubeconfig --name dev-chain-platform-eks --profile chain
+aws eks update-kubeconfig --name stage-chain-platform-eks --profile chain
+aws eks update-kubeconfig --name prod-chain-platform-eks --profile chain
+
+aws eks update-kubeconfig --name dev-galachain
+aws eks update-kubeconfig --name stage-galachain
+aws eks update-kubeconfig --name prod-galachain
