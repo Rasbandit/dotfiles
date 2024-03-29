34f79acc19baf5569923c9f03a0cd8001a99035c
rasbandit <todd.rasband@gmail.com>
2024-03-28 23:50:02 -0600

    2024-03-28 23:50

--- .setup-scripts/apt/exodus.sh
+++ .setup-scripts/apt/exodus.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-# wget https://zoom.us/client/latest/zoom_amd64.deb
-# sudo apt install -y ./zoom_amd64.deb
-
-wget -O ./exodus.deb https://downloads.exodus.com/releases/exodus-linux-x64-24.13.3.deb
-sudo apt install -y ./exodus.deb
+# wget -O ./exodus.deb https://downloads.exodus.com/releases/exodus-linux-x64-24.13.3.deb
+# sudo apt install -y ./exodus.deb
---
+++ .setup-scripts/apt/steam.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo apt update -y && sudo apt upgrade -y
+sudo add-apt-repository multiverse -y
+sudo apt update -y
+sudo apt install steam -y
