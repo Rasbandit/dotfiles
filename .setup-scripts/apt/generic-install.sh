0a828840e9384dda303ed34eaf24055b61824388
rasbandit <todd.rasband@gmail.com>
2024-03-28 19:00:02 -0600

    2024-03-28 19:00

--- .setup-scripts/apt/generic-install.sh
+++ .setup-scripts/apt/generic-install.sh
@@ -13,11 +13,12 @@ vlc \
 pavucontrol
 
 sudo snap refresh
-sudo snap install discord todoist zoom-client
+sudo snap install discord todoist
 sudo snap set core experimental.refresh-app-awareness=true
 
 ~/.setup-scripts/other-installs/nordvpn.sh
 ~/.setup-scripts/other-installs/droidcam.sh
+~/.setup-scripts/apt/zoom.sh
 
 ## INSOMNIA/postman
 ## Wireguard
---
+++ .setup-scripts/apt/zoom.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wget https://zoom.us/client/latest/zoom_amd64.deb
+sudo apt install ./zoom_amd64.deb
+rm zoom_amd64.deb
