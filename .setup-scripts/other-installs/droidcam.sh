d4072a3d2892e5ceb489220ee7e51c71401c244f
rasbandit <todd.rasband@gmail.com>
2024-03-28 18:30:04 -0600

    2024-03-28 18:30

--- .setup-scripts/other-installs/droidcam.sh
+++ .setup-scripts/other-installs/droidcam.sh
@@ -1,1 +1,8 @@
 #!/bin/bash
+
+wget -O droidcam_latest.zip https://files.dev47apps.net/linux/droidcam_2.1.3.zip
+# sha1sum: 2646edd5ad2cfb046c9c695fa6d564d33be0f38b
+unzip droidcam_latest.zip -d droidcam
+cd droidcam && sudo ./install-client
+rm droidcam_latest.zip 
+sudo apt install linux-headers-`uname -r` gcc make
