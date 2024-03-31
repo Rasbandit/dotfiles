595d4e38bb730596d900f027adc9a4d8abae8748
rasbandit <todd.rasband@gmail.com>
2024-03-31 01:20:03 -0600

    2024-03-31 01:20

--- .setup-scripts/apt/obsidian.sh
+++ .setup-scripts/apt/obsidian.sh
@@ -1,24 +1,29 @@
 #!/bin/bash
 
-# wget https://github.com/obsidianmd/obsidian-releases/releases/latest
-# apt install -y obsidian_1.5.11_amd64.deb
+# GitHub repository details
+OWNER="obsidianmd"
+REPO="obsidian-releases"
 
-# GitHub repository and release URL
-REPO_OWNER="obsidianmd"
-REPO_NAME="obsidian-releases"
-RELEASE_URL="https://github.com/$REPO_OWNER/$REPO_NAME/releases/latest"
+# File extension to download
+FILE_EXTENSION=".deb"
 
-# Fetch the release page HTML
-HTML=$(curl -sSL $RELEASE_URL)
+# URL for latest release
+URL="https://api.github.com/repos/$OWNER/$REPO/releases/latest"
 
-# Extract the download URL for the latest release ending with "_amd64.deb"
-DOWNLOAD_URL=$(echo "$HTML" | xmllint --html --xpath '//a[contains(@href, "_amd64.deb")]/@href' - 2>/dev/null | cut -d '"' -f 2)
+# Fetch release information
+response=$(curl -sSL "$URL")
 
-# Download the release
-if [ -n "$DOWNLOAD_URL" ]; then
-    echo "Downloading latest release from $DOWNLOAD_URL"
-    # wget "https://github.com$DOWNLOAD_URL"
+# Parse assets URL and download the file
+asset_url=$(echo "$response" | jq -r ".assets[] | select(.name | endswith(\"$FILE_EXTENSION\")) | .browser_download_url")
+
+if [ -n "$asset_url" ]; then
+    echo "Downloading: $asset_url"
+    wget -O "obsidian.deb" "$asset_url"
+    echo "Download complete."
+
+    sudo apt install ./obsidian.deb -y
+
+    rm ./obsidian.deb
 else
-    echo "Failed to extract download URL for _amd64.deb file."
-    exit 1
+    echo "No release found with the specified file extension."
 fi
