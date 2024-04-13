ca00fd7f1640cafff8f299808a5b6b5eb4988509
rasbandit <todd.rasband@gmail.com>
2024-04-13 22:30:06 +0900

    2024-04-13 22:30

--- .setup-scripts/apt/slack.sh
+++ .setup-scripts/apt/slack.sh
@@ -1,1 +1,5 @@
 #!/bin/bash
+
+# echo wget "https://downloads.slack-edge.com/linux_releases/slack-desktop-$(curl -s "https://slack.com/intl/es/release-notes/linux" | xmllint --html --xpath '//h2' - 2>/dev/null | head -n1 | sed 's/<h2>//;s#</h2>##;s/Slack //')-amd64.deb"
+
+curl -s https://slack.com/release-notes/linux | sed -n "/^.*<h2>Slack /{;s///;s/[^0-9.].*//p;q;}"
