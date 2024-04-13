c102586a78c230fe7aceccd94b7d2713239ee1f7
rasbandit <todd.rasband@gmail.com>
2024-04-13 22:20:38 +0900

    2024-04-13 22:20

--- .setup-scripts/job-specific/gala.sh
+++ .setup-scripts/job-specific/gala.sh
@@ -7,10 +7,9 @@ sudo snap install slack
 ~/.setup-scripts/apt/lens.sh
 ~/.setup-scripts/apt/tailscale.sh
 ~/.setup-scripts/other-installs/kubectl.sh
+~/.setup-scripts/other-installs/aws-cli.sh
 
 ~/.setup-scripts/mount-secret-envar.sh "Gala" "Gala Envars"
-
-~/.setup-scripts/other-installs/aws-cli.sh
 
 aws configure set aws_access_key_id "$(op item get "Gala AWS New" --fields label=access_key_id)" --profile chain
 aws configure set aws_secret_access_key "$(op item get "Gala AWS New" --fields label=secret_access_key)" --profile chain
