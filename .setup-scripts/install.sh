7cb7603dfb3aca911c9b99f856085a0e26977336
rasbandit <todd.rasband@gmail.com>
2024-04-17 21:50:01 +0900

    2024-04-17 21:50

--- .setup-scripts/install.sh
+++ .setup-scripts/install.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 
-touch ~/.bash_secrets
+# touch ~/.bash_secrets
+
+OS_INFO=$(cat /etc/os-release)
+
+# Use grep to filter out the line containing ID= and then use cut to extract the value
+OS=$(echo "$OS_INFO" | grep "^ID=" | cut -d= -f2)
+
+# Print the value of the ID variable
+echo "$id"
 
 # Check if the distribution is Ubuntu
-if [ "$(lsb_release -si)" = "Ubuntu" ]; then
+if [ $OS = "ubuntu" ]; then
     ~/.setup-scripts/operating-systems/ubuntu-setup.sh
 fi
 
-# LATER USE grep -E '^(NAME)=' /etc/os-release 
+if [ $OS = "debian" ]; then
+    ~/.setup-scripts/operating-systems/debian-setup.sh
+fi
 
-# Path to your script
-# TODO this crontab is not default installed on fedora
 script_path="/home/${USER}/.setup-scripts/cron-jobs/auto-add-push.sh"
 cron_job="*/10 * * * * $script_path"
 echo "$cron_job" > /tmp/cron_job
