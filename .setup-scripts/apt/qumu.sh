e56f395c63be58820af55e4fe4314cedcd405dc2
rasbandit <todd.rasband@gmail.com>
2024-04-17 22:50:02 +0900

    2024-04-17 22:50

---
+++ .setup-scripts/apt/qumu.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo apt install qemu-kvm qemu-system qemu-utils python3 python3-pip libvirt-clients libvirt-daemon-system bridge-utils virtinst libvirt-daemon virt-manager -y
+
+sudo virsh net-start default
+sudo virsh net-autostart default
+
+sudo usermod -aG libvirt $USER
+sudo usermod -aG libvirt-qemu $USER
+sudo usermod -aG kvm $USER
+sudo usermod -aG input $USER
+sudo usermod -aG disk $USER
