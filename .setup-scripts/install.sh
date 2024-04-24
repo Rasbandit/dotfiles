7fc3d89ce826eb80773d1e2373034e3fcc1fd0a9
rasbandit <todd.rasband@gmail.com>
2024-04-24 22:30:03 +0900

    2024-04-24 22:30

--- .setup-scripts/install.sh
+++ .setup-scripts/install.sh
@@ -2,21 +2,27 @@
 
 # touch ~/.bash_secrets
 
-OS_INFO=$(cat /etc/os-release)
+mkdir -p ~/.local/share/fonts
+curl -L -o ~/.icons/papirus.tar.xz "https://drive.google.com/uc?export=download&id=1V3oPRCZwKLVXN24-T8vIV7ojecmpofUJ"
+tar -xf ~/.icons/papirus.tar.xz -C ~/.icons
+gsettings set org.gnome.desktop.interface icon-theme "Papirus"
+rm ~/.icons/papirus.tar.xz
 
-OS=$(echo "$OS_INFO" | grep "^ID=" | cut -d= -f2)
+# OS_INFO=$(cat /etc/os-release)
 
-# Check if the distribution is Ubuntu
-if [ $OS = "ubuntu" ]; then
-    ~/.setup-scripts/operating-systems/ubuntu-setup.sh
-fi
+# OS=$(echo "$OS_INFO" | grep "^ID=" | cut -d= -f2)
 
-if [ $OS = "debian" ]; then
-    ~/.setup-scripts/operating-systems/debian-setup.sh
-fi
+# # Check if the distribution is Ubuntu
+# if [ $OS = "ubuntu" ]; then
+#     ~/.setup-scripts/operating-systems/ubuntu-setup.sh
+# fi
 
-script_path="/home/${USER}/.setup-scripts/cron-jobs/auto-add-push.sh"
-cron_job="*/10 * * * * $script_path"
-echo "$cron_job" > /tmp/cron_job
-crontab /tmp/cron_job
-rm /tmp/cron_job
+# if [ $OS = "debian" ]; then
+#     ~/.setup-scripts/operating-systems/debian-setup.sh
+# fi
+
+# script_path="/home/${USER}/.setup-scripts/cron-jobs/auto-add-push.sh"
+# cron_job="*/10 * * * * $script_path"
+# echo "$cron_job" > /tmp/cron_job
+# crontab /tmp/cron_job
+# rm /tmp/cron_job
