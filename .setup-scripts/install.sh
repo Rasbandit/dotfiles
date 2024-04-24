5a1b7a5eaa4d4e7440ffbce240d3e3c4faf54259
rasbandit <todd.rasband@gmail.com>
2024-04-24 23:00:03 +0900

    2024-04-24 23:00

--- .setup-scripts/install.sh
+++ .setup-scripts/install.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
-# touch ~/.bash_secrets
+touch ~/.bash_secrets
 
 mkdir -p ~/.local/share/fonts
 curl -L -o ~/.local/share/fonts/fonts.tar.gz "https://drive.google.com/uc?export=download&id=1Y6pr7AVh_WvU0jOLPscu_T8PvX8cxmfr"
 tar -xf ~/.local/share/fonts/fonts.tar.gz -C ~/.local/share/fonts
 rm ~/.local/share/fonts/fonts.tar.gz
 
-# OS_INFO=$(cat /etc/os-release)
+OS_INFO=$(cat /etc/os-release)
 
-# OS=$(echo "$OS_INFO" | grep "^ID=" | cut -d= -f2)
+OS=$(echo "$OS_INFO" | grep "^ID=" | cut -d= -f2)
 
-# # Check if the distribution is Ubuntu
-# if [ $OS = "ubuntu" ]; then
-#     ~/.setup-scripts/operating-systems/ubuntu-setup.sh
-# fi
+# Check if the distribution is Ubuntu
+if [ $OS = "ubuntu" ]; then
+    ~/.setup-scripts/operating-systems/ubuntu-setup.sh
+fi
 
-# if [ $OS = "debian" ]; then
-#     ~/.setup-scripts/operating-systems/debian-setup.sh
-# fi
+if [ $OS = "debian" ]; then
+    ~/.setup-scripts/operating-systems/debian-setup.sh
+fi
 
-# script_path="/home/${USER}/.setup-scripts/cron-jobs/auto-add-push.sh"
-# cron_job="*/10 * * * * $script_path"
-# echo "$cron_job" > /tmp/cron_job
-# crontab /tmp/cron_job
-# rm /tmp/cron_job
+script_path="/home/${USER}/.setup-scripts/cron-jobs/auto-add-push.sh"
+cron_job="*/10 * * * * $script_path"
+echo "$cron_job" > /tmp/cron_job
+crontab /tmp/cron_job
+rm /tmp/cron_job
