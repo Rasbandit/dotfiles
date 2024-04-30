cef2a4c55cd698703d735c77389b84cb374bcd56
rasbandit <todd.rasband@gmail.com>
2024-04-30 23:40:02 +0900

    2024-04-30 23:40

--- .setup-scripts/apt/nodejs.sh
+++ .setup-scripts/apt/nodejs.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-sudo apt update
-## Todo always get latest
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+if [ "$SHELL" = "/usr/bin/fish" ]; then
+    curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
+elif [ "$SHELL" = "/bin/bash" ] || [ "$SHELL" = "/usr/bin/bash" ]; then
+    sudo apt update
+    ## Todo always get latest
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+else
+    echo "Current shell is neither Fish nor Bash"
+fi
