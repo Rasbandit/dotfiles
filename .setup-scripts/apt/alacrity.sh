da9871b9c863477ab0433187cb8c011561c0eee0
rasbandit <todd.rasband@gmail.com>
2024-04-30 22:20:02 +0900

    2024-04-30 22:20

---
+++ .setup-scripts/apt/alacrity.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo apt update && sudo apt upgrade
+sudo add-apt-repository ppa:aslatter/ppa -y
+sudo apt install alacritty
+
+sudo apt install cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3 -y
+
+sudo curl https://sh.rustup.rs -sSf | sh
+
+git clone https://github.com/jwilm/alacritty.git
