2253444b679236eafc0958c6023d8c2c28fd9a18
rasbandit <todd.rasband@gmail.com>
2024-05-22 03:00:05 +0900

    2024-05-22 03:00

--- .setup-scripts/job-specific/gala.sh
+++ .setup-scripts/job-specific/gala.sh
@@ -15,18 +15,8 @@ tailscale
 
 ~/.setup-scripts/mount-secret-envar.sh "Gala" "Gala Envars"
 
-aws configure set aws_access_key_id "$(op item get "Gala AWS New" --fields label=access_key_id)" --profile chain
-aws configure set aws_secret_access_key "$(op item get "Gala AWS New" --fields label=secret_access_key)" --profile chain
-aws configure set region us-east-1 --profile chain
-aws configure set output json --profile chain
+aws sso login
 
-aws configure set aws_access_key_id "$(op item get "Gala AWS" --fields label=access_key_id)"
-aws configure set aws_secret_access_key "$(op item get "Gala AWS" --fields label=secret_access_key)"
-aws configure set region us-east-1
-aws configure set output json
-
-aws eks update-kubeconfig --name dev-chain-platform-eks --profile chain
-aws eks update-kubeconfig --name stage-chain-platform-eks --profile chain
-aws eks update-kubeconfig --name prod-chain-platform-eks --profile chain
-
-aws eks update-kubeconfig --name prod-galachain
+aws eks update-kubeconfig --name dev-chain-platform-eks
+aws eks update-kubeconfig --name stage-chain-platform-eks
+aws eks update-kubeconfig --name prod-chain-platform-eks
