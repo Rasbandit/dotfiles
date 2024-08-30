7f7e2d6d28ab7a5b55f7e6af804431769fe7a8c0
rasbandit <todd.rasband@gmail.com>
2024-08-30 02:20:06 -0600

    2024-08-30 02:20

--- .setup-scripts/other-installs/kanata.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-git clone git@github.com:jtroo/kanata.git /tmp/kanata
-
-cargo install kanata --path /tmp/kanata
-systemctl --user enable kanata.service
---
+++ .setup-scripts/other-installs/xremap.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+git clone https://github.com/xremap/xremap.git /tmp/xremap
+
+cargo install xremap --path /tmp/xremap
