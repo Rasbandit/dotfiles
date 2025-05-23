d5000e0212249aa77570695a2a0fac233b223602
rasbandit <todd.rasband@gmail.com>
2025-05-23 00:04:50 -0600

    2025-05-23 00:04

--- .setup-scripts/dnf/tailscale.sh
+++ .setup-scripts/dnf/tailscale.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/focal.noarmor.gpg | sudo tee /usr/share/keyrings/tailscale-archive-keyring.gpg >/dev/null
-curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/focal.tailscale-keyring.list | sudo tee /etc/apt/sources.list.d/tailscale.list
+sudo dnf config-manager addrepo --from-repofile=https://pkgs.tailscale.com/stable/fedora/tailscale.repo
+sudo dnf install tailscale
 
-sudo apt-get update
-sudo apt-get install tailscale
+sudo systemctl enable --now tailscaled
