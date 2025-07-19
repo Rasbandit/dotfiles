506e5a6fb16779deb673fbb4f70fb5577b0add65
rasbandit <todd.rasband@gmail.com>
2025-07-19 17:00:06 -0600

    2025-07-19 17:00

--- .setup-scripts/dnf/specific-apps/xbox-dogle-setup.sh
+++ .setup-scripts/dnf/specific-apps/xbox-dogle-setup.sh
@@ -5,4 +5,3 @@ sudo dnf install xone lpf-xone-firmware
 sudo lpf approve xone-firmware
 sudo lpf build xone-firmware
 sudo lpf install xone-firmware
-reboot
--- .setup-scripts/operating-systems/fedora-setup.sh
+++ .setup-scripts/operating-systems/fedora-setup.sh
@@ -16,5 +16,4 @@ sudo dnf install -y flatpak
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
-
 ~/.setup-scripts/dnf/generic-install.sh
