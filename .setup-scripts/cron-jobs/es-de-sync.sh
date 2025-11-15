c4520613dffdb2dbfbc24bac130a564d8d776e3a
rasbandit <todd.rasband@gmail.com>
2025-11-15 14:07:30 -0700

    2025-11-15 14:07

--- .setup-scripts/cron-jobs/es-de-sync.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-SRC_DIR="/home/rasbandit/ES-DE/"
-DST_DIR="/mnt/es-de-nas/"
-rsync -av --delete --no-perms --no-owner --no-group --omit-dir-times --exclude 'roms/' "$SRC_DIR" "$DST_DIR"
