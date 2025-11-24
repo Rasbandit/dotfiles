f1c92ec38d061bd685b54ee3bc044b0a0b053223
rasbandit <todd.rasband@gmail.com>
2025-11-23 20:38:22 -0700

    2025-11-23 20:38

--- .setup-scripts/fedora/dnf/gaming-setup.sh
+++ .setup-scripts/fedora/dnf/gaming-setup.sh
@@ -28,6 +28,10 @@ sudo flatpak install org.gnome.World.PikaBackup -y
 sudo flatpak install flathub org.DolphinEmu.dolphin-emu -y
 sudo flatpak install flathub org.libretro.RetroArch -y
 
+# Udev to ignore keycrhon
+echo 'KERNEL=="event*", SUBSYSTEM=="input", ENV{ID_VENDOR_ID}=="3434", ENV{ID_INPUT_JOYSTICK}=="*?", ENV{ID_INPUT_JOYSTICK}="""' | sudo tee /etc/udev/rules.d/99-keychron-no-joysticks.rules >/dev/null 
+sudo udevadm control --reload-rules && sudo udevadm trigger --subsystem-match=input
+
 #Auto Starts
 ~/.setup-scripts/scripts/autostart-link.sh link es-de.desktop
 ~/.setup-scripts/scripts/autostart-link.sh link steam.desktop
