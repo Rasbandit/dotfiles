335c941e8da9e9f2a7909579107a41223d98a7a0
rasbandit <todd.rasband@gmail.com>
2025-11-25 22:47:03 -0700

    2025-11-25 22:47

--- .setup-scripts/desktop-environments/gnome.sh
+++ .setup-scripts/desktop-environments/gnome.sh
@@ -92,6 +92,7 @@ gsettings set org.gnome.desktop.wm.keybindings switch-applications []
 gsettings set org.gnome.desktop.wm.keybindings switch-applications-backward []
 gsettings set org.gnome.desktop.wm.keybindings switch-windows "['<Alt>Tab']"
 gsettings set org.gnome.desktop.wm.keybindings switch-windows-backward "['<Shift><Alt>Tab']"
+gsettings set org.gnome.mutter experimental-features "['variable-refresh-rate']"
 
 # Font
 gsettings set org.gnome.desktop.interface monospace-font-name 'MonoLisa Nerd Font Regular 13'
